63ba7290eb970d2bb5de7ad4c2e221810107a6cd
Megan Pekarek <mpekarek@fishbowlsolutions.com>
2021-01-14 18:35:12 -0600

    add player page

--- src/views/AddPlayer.js
+++ src/views/AddPlayer.js
@@ -1,5 +1,6 @@
 import '../App.css';
 import React from 'react'
+import PrimaryButton from '../components/PrimaryButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRandom, faUsers } from '@fortawesome/free-solid-svg-icons'
 
@@ -11,7 +12,13 @@ class AddPlayer extends React.Component {
   render() {
     return (
       <div className="App">
-        add player
+        <span className="pageHeader">Player 1</span>
+        <div className="playerFormWrapper">
+          <input placeholder="name"></input>
+          <div className="addPlayerBtnWrapper">
+            <PrimaryButton btnTitle="Enter Phrases" />
+          </div>
+        </div>
       </div>
     );
   }
